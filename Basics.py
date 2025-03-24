cff0109a9adb400db11a1e5958769cbc17282b84
Sad <amanatrish123@gmail.com>
2025-03-24 21:02:47 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,51 @@
+#Variable = A container for a value (string, integer, float, boolean)
+# A variable behaves as if it was the value it contains
+
+#Strings
+first_name = "Aman"
+# print(first_name)
+food = "Murruku"
+email = "legend123@gmail.com"
+
+
+#FOR OUPUT
+print(f"Hello {first_name}")
+print(f"She likes {food}")
+print(f"you can contact me on {email}")
+
+
+
+#Integer 
+age = 19
+quantity = 20
+
+print(f"You're {age} years old")
+print(f"You're buying {quantity} items")
+
+
+
+#Float 
+price = 9.99
+gpa = 3.5
+distance = 10.5
+
+print(f"The price is ${price}")
+print(f"Your GPA is: {gpa}")
+print(f"You ran {distance}km")
+
+
+#Boolean
+
+is_student = False
+for_sale = True 
+
+# if is_student:
+#     print("You're a student !")
+# else:
+#     print("You're not a student !")
+# print(f"Are u a student ? {is_student}")
+
+if for_sale:
+    print("That item is for sale !")
+else:
+    print("That item is not for sale !")
