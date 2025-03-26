e3252973c749fb57010313f24abec678af8ede8b
Sadhak <amanatrish123@gmail.com>
2025-03-26 20:00:11 +0530

    Add files via upload

---
+++ madlibsgame.py
@@ -0,0 +1,12 @@
+#madlibsgame - a game where we creates a story and fill the blanks with random words
+
+adjective1 = input("Enter a adjective(description): ")
+noun1 = input("Enter a noun(person,place,thing): ")
+adjective2 = input("Enter a adjective(description): ")
+verb1 = input("Enter a verb(ing form): ")
+adjective3 = input("Enter a adjective(description): ")
+
+print(f"Today I went to a {adjective1} Hospital")
+print(f"There was a {noun1} , who was {verb1}")
+print(f"I tried talking to him but he was {adjective3}")
+print(f"Still , i was very {adjective2} to talk to him")
